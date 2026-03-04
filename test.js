e09438a5658a82d8c10d14e7f45d34cc7071ba1e
ayct-16 <yessenia.cordova.tapia@gmail.com>
2026-03-04 13:37:41 +0000

    modificado

--- test.js
+++ test.js
@@ -9,3 +9,17 @@ test('adds 14 + 9 to equal 23', () => {
     // Esperamos que la suma de esos 2 números sea 23
     expect(total).toBe(23);
 });
+
+test("One euro should be 1.07 dollars", function() {
+    // Importo la funcion desde app.js
+    const { fromEuroToDollar } = require('./app.js');
+
+    // Uso la función como debe ser usada
+    const dollars = fromEuroToDollar(3.5);
+
+    // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
+    const expected = 3.5 * 1.07;
+
+    // Hago mi comparación (la prueba)
+    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+})
